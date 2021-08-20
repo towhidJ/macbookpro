c3b934434abd87ed6c57ec099fe0e60002af139e
towhidJ <towhidulislamcse@gmail.com>
2021-08-21 01:32:13 +0600

    update total

---
+++ js/app.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+function updateCost(cost,price,costType)
+{
+    const costTotal = document.getElementById(cost);
+    costTotal.innerText = price;
+    updateTotalPrice();
+}
+
+function updateTotalPrice()
+{
+    const totalPrice = document.getElementById('total-price');
+    const bestPrice = document.getElementById('best-price').innerText;
+    const extraMemoryPrice = document.getElementById('extra-memory-cost').innerText;
+    const extraStroagePrice = document.getElementById('extra-stroage-cost').innerText;
+    const extraDeliveryPrice = document.getElementById('delivery-charge').innerText;
+    totalPrice.innerText = parseInt(bestPrice)+parseInt(extraMemoryPrice)+parseInt(extraStroagePrice)+parseInt(extraDeliveryPrice);
+
+
+}
+
+// -------------------------------memory handler change--------------------------
+
+// 8gb memory handler
+document.getElementById('memory-btn-8gb').addEventListener('click', 
+function(){
+    updateCost('extra-memory-cost',0);
+});
+
+// 16gb memory handler
+document.getElementById('memory-btn-16gb').addEventListener('click', 
+function(){
+    updateCost('extra-memory-cost',180);
+});
+
+// -------------------------------stroage handler change--------------------------
+
+
+// 256gb Stroage handler
+document.getElementById('ssd-btn-256gb').addEventListener('click', 
+function(){
+    updateCost('extra-stroage-cost',0);
+});
+
+// 512gb stroage handler
+document.getElementById('ssd-btn-512gb').addEventListener('click', 
+function(){
+    updateCost('extra-stroage-cost',100);
+});
+
+// 1tb stroage handler
+document.getElementById('ssd-btn-1tb').addEventListener('click', 
+function(){
+    updateCost('extra-stroage-cost',180);
+});
+
+
+// -------------------------------Delivery handler change--------------------------
+
+// 8gb memory handler
+document.getElementById('free-delivery').addEventListener('click', 
+function(){
+    updateCost('delivery-charge',0);
+});
+
+// 16gb memory handler
+document.getElementById('paid-delivery').addEventListener('click', 
+function(){
+    updateCost('delivery-charge',20);
+});
